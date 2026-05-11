44191363132735f8e7a552bc3fb44bc4e972e3a9
Josh Gilstorf <gilstorfj2@mailbox.winthrop.edu>
2026-05-11 15:29:07 -0400

    Added centralized post data file, and renamed byte takes and play log folders.

---
+++ src/data/posts.ts
@@ -0,0 +1,30 @@
+export const posts = [
+    {
+        slug: "byte-take-star-fox-reboot",
+        title: "Why Rebooting Star Fox is the Right Move",
+        excerpt: "As annoying as it can be to recieve yet another retelling of Star Fox 64, a fresh start is exactly what this franchise needs.",
+        category: "Byte Take",
+        date: "May 6th, 2026"
+    },
+    {
+        slug: "review-tomodachi-life-living-the-dream",
+        title: "REVIEW: Tomodachi Life: Living the Dream is a Near Perfect Sequel",
+        excerpt: "I've wanted a new Tomodachi Life for a long time, and I don't think I could've asked for much more from what we got.",
+        category: "Review",
+        date: "May 1st, 2026"
+    },
+    {
+        slug: "news-yoshi-retail-demo",
+        title: "Yoshi and the Mysterious Book Gets Retail Demo",
+        excerpt: "The intriguing, cozy platformer is available to try at select retailers two weeks ahead of release.",
+        category: "News",
+        date: "May 8th, 2026"
+    },
+    {
+        slug: "play-log-5-3-26",
+        title: "What I've Been Playing: Week of 5/3/2026",
+        excerpt: "Spoiler Alert: I'm still Living the Dream.",
+        category: "Play Log",
+        date: "May 9th, 2026"
+    }
+]
