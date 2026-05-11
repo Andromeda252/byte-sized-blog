c2f38e39cdfd0b7ab8a89262397799e34871270b
Josh Gilstorf <gilstorfj2@mailbox.winthrop.edu>
2026-05-11 17:39:37 -0400

    Made post cards clickable by wrapping them in a 'Link' element. Currently lead to a 404 page.

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,12 +1,15 @@
 import { Post } from "@/types/post"
+import Link from "next/link"
 
-export default function PostCard({ title, excerpt, category, date }: Post) {
+export default function PostCard({ slug, title, excerpt, category, date }: Post) {
     return (
-        <article className="border border-zinc-200 rounded-xl p-6 mb-4 bg-white shadow-sm hover:shadow-md transition-shadow duration-200">
-            <small className="text-sm text-purple-600 font-medium">{category}</small>
-            <h3 className="text-2xl font-bold mt-2 mb-3">{title}</h3>
-            <p className="text-zinc-600 leading-relaxed">{excerpt}</p>
-            <small>{date}</small>
-        </article>
+        <Link href={`/posts/${slug}`}>
+            <article className="border border-zinc-200 rounded-xl p-6 mb-4 bg-white shadow-sm hover:shadow-md transition-shadow duration-200">
+                <small className="text-sm text-purple-600 font-medium">{category}</small>
+                <h3 className="text-2xl font-bold mt-2 mb-3">{title}</h3>
+                <p className="text-zinc-600 leading-relaxed mb-4">{excerpt}</p>
+                <small className="text-zinc-500">{date}</small>
+            </article>
+        </Link>
     )
 }
