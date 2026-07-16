8a46817d5ab20ff4db9d85e0981eca36204cf676
Josh Gilstorf <gilstorfj2@mailbox.winthrop.edu>
2026-07-15 20:56:32 -0400

    Made some alterations to the About Page to better reflect the current state of the project. I plan to update it later when I have more time to work on it.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,8 +24,8 @@ export default function AboutPage() {
                     discourse seeming to skew more and more negative by the day, that my blog would focus on what makes
                     gaming great. That may not seem all that revolutionary, but my aim is to do it in a way that injects
                     into the gaming landscape a genuine sense passion, enthusiasm, and most of all, optimism that I feel
-                    it could use more of. I also aim to not waste your time with overbloated articles by aiming to keep most
-                    content short-form, hence the 'Byte-Sized' in the name. If you love gaming and desire to spend more time 
+                    it could use more of. {/* I also aim to not waste your time with overbloated articles by aiming to keep most
+                    content short-form, hence the 'Byte-Sized' in the name.  */} If you love gaming and desire to spend more time 
                     celebrating the good and less time stewing over the bad, you've come to the right place.
                 </p>
                 <h2 className="text-2xl md:text-3xl font-bold mb-6 text-red-700">
@@ -41,8 +41,8 @@ export default function AboutPage() {
                     (with the sole exception of Fire Emblem, just haven't gotten around to trying it yet), and have thoroughly enjoyed my time
                     with each. However, contrary to what it may seem, I'm not all Nintendo all the time. I also have a PS5, and greatly enjoy many
                     non-Nintendo franchises, such as (but not limited to) Sonic the Hedgehog, Kingdom Hearts, Final Fantasy, and, interestingly enough,
-                    Five Nights at Freddy's (my favorite series I've never played cause I'm a wuss lmao). If you're interested in learning more
-                    about what I'm into, I'd recommend the following article on my favorite games!
+                    Five Nights at Freddy's (my favorite series I've never played cause I'm too scared lmao). {/* If you're interested in learning more
+                    about what I'm into, I'd recommend the following article on my favorite games! */}
                 </p>
         </PageContainer>
     )
